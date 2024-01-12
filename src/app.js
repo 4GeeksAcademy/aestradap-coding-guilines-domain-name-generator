a895f5317ca841f2bb9430f327ee95fce1808c16
aestradap <55397157+aestradap@users.noreply.github.com>
2024-01-12 04:37:59 +0000

    all the functins have feedback from the console, the excercise its complete

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  const generateCombis = () => {
+    for (const pronounValue of pronoun) {
+      for (const adjValue of adj) {
+        for (const nounValue of noun) {
+          console.log(pronounValue + adjValue + nounValue + ".com");
+        }
+      }
+    }
+    return undefined;
+  };
+
+  function asyncCall() {
+    console.log("The possible domain name combinations from this lists are...");
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve("slow");
+        generateCombis();
+      }, 2000);
+    });
+  }
+
+  console.log("Hello, this is the Domain Name Generator exercise.");
+  console.log("Using: ");
+  console.log(pronoun);
+  console.log(adj);
+  console.log(noun);
+  asyncCall();
 };
